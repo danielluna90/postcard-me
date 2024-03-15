7eeafe5b5e648ef81dbaf4751a038bac66241bf6
Daniel Luna <l2318dan@gmail.com>
2024-03-14 20:25:24 -0400

    Finalize outline of postcard

--- app/components/postcard.tsx
+++ app/components/postcard.tsx
@@ -8,8 +8,23 @@ export default function PostCard() {
         <table className="w-full h-full table-fixed">
           <tbody>
             <tr>
-              <td className="border-r-black border-r-2"></td>
-              <td></td>
+              <td className="h-full border-r-black border-r-2">
+                <div id="message" className="w-full h-full">
+                  <p className="text-[12px] text-center">THIS SPACE FOR WRITING MESSAGES</p>
+                  <p className="width-full mr-8"></p>
+                  
+                </div>
+              </td>
+              <td className="h-full flex flex-col justify-between items-end">
+                <div id="stamp" className="w-[0.87in] h-[0.98in] bg-black"></div>
+                <div id="address-lines" className="w-full h-[2.25in] py-[0.25in] flex flex-col justify-between">
+                  <p className="text-[12px] text-center  ml-8 mr-4">THIS SPACE FOR ADDRESS ONLY</p>
+                  <hr className="h-0.5 ml-8 mr-4 bg-black border-0" />
+                  <hr className="h-0.5 ml-8 mr-4 bg-black border-0" />
+                  <hr className="h-0.5 ml-8 mr-4 bg-black border-0" />
+                  <hr className="h-0.5 ml-8 mr-4 bg-black border-0" />
+                </div>
+              </td>
             </tr>
           </tbody>
         </table>
